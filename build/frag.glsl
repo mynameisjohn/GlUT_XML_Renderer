fc8f220251d828f6e8dddeb64fc9ee571fe0e4b6
mynameisjohn <mynameisjohnj@gmail.com>
2015-07-09 23:56:10 -0400

    Commented Shaders

--- build/frag.glsl
+++ build/frag.glsl
@@ -17,6 +17,7 @@ varying vec3 v_Nrm;
 varying vec3 v_Pos;
 
 void main(){
+	// Compute total light contribution from all lights, all diffuse for now
 	vec3 light;
 	for (int i = 0; i < NUM_LIGHTS; i++){
 		if (L[i].type == 0) // directional
@@ -25,11 +26,11 @@ void main(){
 			light += nDotVP * L[i].intensity;
 		}
 		else if (L[i].type == 1){ // point
-			vec3 VP = -(L[i].pos - v_Pos);
+			vec3 VP = (L[i].pos - v_Pos);
 			float d = length(VP);
 			float nDotVP = max(0, dot(v_Nrm, normalize(VP)));
-			float attenuation = 1.0;/*/
-				(L[i].dir[0] + L[i].dir[1] * d + L[i].dir[2] * d*d);*/
+			float attenuation = 1.0 /
+				(L[i].dir[0] + L[i].dir[1] * d + L[i].dir[2] * d*d);
 			light += L[i].intensity * attenuation * nDotVP;
 		}
 		else if (L[i].type == 2){ // ambient
--- build/vert.glsl
+++ build/vert.glsl
@@ -2,6 +2,7 @@
 
 #define NUM_LIGHTS 3
 
+// Definition of a Light Struct; guess how many there are
 struct Light{
 	int type;
 	vec3 pos;
@@ -9,20 +10,27 @@ struct Light{
 	vec3 intensity;
 };
 
+// Where I keep 'em
 uniform Light L[NUM_LIGHTS];
 
+// Model View, Projection
 uniform mat4 MV;
 uniform mat4 P;
 
+// Vertex Attributes for position, normal; more to come
 attribute vec3 a_Pos;
 attribute vec3 a_Nrm;
 
+// I interpolate them for lighting calculations; is this common?
 varying vec3 v_Nrm;
 varying vec3 v_Pos;
 
 void main(){
+	// Find position in world space, for lighting calc
 	vec4 w_Pos = MV * vec4(a_Pos, 1);
+	// Screen space
 	gl_Position = P * w_Pos;
+	// Interpolate
 	v_Nrm = a_Nrm;
 	v_Pos = w_Pos.xyz;
 }
