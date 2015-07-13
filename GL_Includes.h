98dc82c4c4ca419b2c5f9c9c0ca49444f86205b2
mynameisjohn <mynameisjohnj@gmail.com>
2015-07-13 18:39:06 -0400

    A) Moved Camera, Shader to main and made global, since Scene was getting too big. B) Created a GL_Includes file. First OS X Commit

---
+++ GL_Includes.h
@@ -0,0 +1,20 @@
+//
+//  Header.h
+//  GLUT_XML_Renderer
+//
+//  Created by Cakeadmin on 7/13/15.
+//
+//
+
+#ifndef GL_INCLUDES
+#define GL_INCLUDES
+
+#include <GL/glew.h>
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/freeglut.h>
+#endif
+
+#endif //GL_INCLUDES
